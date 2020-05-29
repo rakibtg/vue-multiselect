97092bd21db04e12eca9261dbeb421c8dcefe593
rakibtg <rakibtg@gmail.com>
2020-05-29 13:39:45 +0600

    Added is-open scope for caret slot

--- src/Multiselect.vue
+++ src/Multiselect.vue
@@ -11,7 +11,7 @@
     class="multiselect"
     role="combobox"
     :aria-owns="'listbox-'+id">
-      <slot name="caret" :toggle="toggle">
+      <slot name="caret" :toggle="toggle" :is-open="isOpen">
         <div @mousedown.prevent.stop="toggle()" class="multiselect__select"></div>
       </slot>
       <slot name="clear" :search="search"></slot>
